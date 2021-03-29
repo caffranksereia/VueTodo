6447c9c9d3ad862711173609422269c735704bce
ferafael <circuncisaofabio@icloud.com>
2021-03-29 18:26:08 -0300

    -Exclusao do parametro todo no service

--- todo/src/Service/Todo.js
+++ todo/src/Service/Todo.js
@@ -1,13 +1,14 @@
 import { api } from './Api';
 
 
+
 export default {
     todoList:() =>{
     
         return api.get('/TodoList')
     },
-    todoSave:(todo) =>{
-        return api.post('/TodoList',todo)
+    todoSave:() =>{
+        return api.post('/TodoList',)
     },
     todoUp:(id) =>{
         return api.put('/TodoList', id)
