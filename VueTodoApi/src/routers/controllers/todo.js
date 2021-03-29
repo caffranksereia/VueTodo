414a649e151eda88fc44892d73c767bf0fa845e6
ferafael <circuncisaofabio@icloud.com>
2021-03-29 18:38:37 -0300

    No controllers-> todo.js aqui estou preparando o terreno para ja adicionar,remover,editar e listar as informações do db.

---
+++ VueTodoApi/src/routers/controllers/todo.js
@@ -0,0 +1,34 @@
+
+exports.get=async(res,req,next)=>{
+    try {
+        const data = await 
+        
+    } catch (error) {
+        res.status
+    }
+}
+
+exports.post=async(res,req,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+exports.delete=async(res,req,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+exports.put=async(res,req,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
