cb8743442f64605863352340a1999ad58b3712a2
ferafael <circuncisaofabio@icloud.com>
2021-03-29 18:35:59 -0300

    Acrescentação da pasta router com o router.js e a const express e express.router. Aqui eu traço o caminho mas ainda não esta pronto apra teste.

---
+++ VueTodoApi/src/routers/Todo.js
@@ -0,0 +1,11 @@
+'user strict';
+const express = require('express')
+const router = express.Router()
+
+router.get('/')
+router.post('/')
+router.delete('/')
+router.put('/')
+
+
+modul.express = router;
