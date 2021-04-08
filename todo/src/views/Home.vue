3a7d4391d5aa0ad946eb502d88eac383ea5380dc
ferafael <circuncisaofabio@icloud.com>
2021-04-07 23:25:00 -0300

    Organizando os spaces em brancos

--- todo/src/views/Home.vue
+++ todo/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <Todo/>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Todo from '@/components/Todo.vue'
-
 export default {
   name: 'Home',
   components: {
