d15f8b3ad165ff8b4cbdc3020d1b67c633d6c0c4
ferafael <circuncisaofabio@icloud.com>
2021-04-07 23:24:41 -0300

    Exclusão do botão Editar pois esta com bug.

--- todo/src/components/Todo.vue
+++ todo/src/components/Todo.vue
@@ -1,30 +1,41 @@
 <template>
-  <div class="todo">
-    
-    <h1>Todo</h1>
-    <div class="tituloTodo">
-      <form action="">
-        <label for="">Titulo</label>
-        <input type="text" name="titulo" id="titulo" v-model="Titulo">
-        <label for="">Todo</label>
-        <input type="text" name="todo" id="todo" v-model="Todo" >
-      </form>
-      <button @click="Adicionar">salvar</button>
+  <div class="todo" style="height=100%;width:100%;">
+    <div class="txt" style="width:100%;height:100px;">
+      <h1>Todo</h1>
     </div>
-    <h1>Lista Todo</h1>
-    <div class="todolist">
-      <ul v-for="todo in todo" :key="todo.id">
-        <form action="">
-          <input type="text" name="Titulo" id="Titulo">
-          <input type="text" name="Todo" id="Todo">
-
-        </form>
-        <li> {{ todo.id }} - {{ todo.Titulo}} - {{ todo.Todo }} - {{todo.done}} - {{ todo.eddit }}</li>
-        <button @click="Editar(todo.id)">Editar</button>
-        <button @click="Excluir(todo.id)">Excluir</button>
+    <div class="tituloTodo" style="height:100px;width:100%;left:50%;">
+      <form>
+        <div class="meu-box">
+          <label class="label-Titulo">Titulo</label>
+          <input type="text" name="titulo" id="titulo" v-model="Titulo" placeholder="Titulo" class="input-Titulo">
+        </div>
+       <div class="meu-box">
+          <label class="label-Todo">Todo</label>
+          <input type="text" name="todo" id="todo" v-model="Todo" placeholder="Todo" class="input-Todo">
+        </div>
+      </form>
+      <button @click="Adicionar" style="width:100px;margin-top:10px">Adicionar</button>
+    </div>
+    <div class="txt" style="width:100%;height:100px;">
+      <h1>Lista Todo</h1>
+    </div>
+    <div class="todolist" style="height:250px;width:100%" >
+      <ul v-for="todo in todo" :key="todo.id" :todo = todo>
+        <div style="width:100%">
+          <li><div>
+                <h3>Titulo</h3>
+                {{ todo.Titulo}} 
+              </div>
+              <div>
+                <h3>Todo </h3>
+                {{ todo.Todo}} 
+              </div>
+          </li>
+        </div>
+        <button @click="Excluir(todo.id)" style="margin-top:10px">Excluir</button>
+        
       </ul>
     </div>
-    
   </div>
 </template>
 
@@ -40,12 +51,9 @@ export default {
     return{
      todos:{
        Titulo:'',
-        Todo:'',
-        done:false,
-        eddit:false
-     },
+       Todo:''
+      },
       todo:[]
-     
     }
   },
   mounted(){
@@ -62,28 +70,24 @@ export default {
       const data = {
         Titulo: this.Titulo,
         Todo: this.Todo,
-        done: this.done,
-        eddit: this.eddit
       }
-      Todo.todoSave(data)
-      this.Listar()
-
-    
-      
-    },
-    Editar(id){
-      Todo.todoUp().then(res=>{
-        
-      })
+      if(this.Todo.trim() !==""){
+        Todo.todoSave(data)
+        this.Listar()
+      }else{
+        this.todo.push(this.Todo),
+        Todo.todoSave(data)
+        this.List();
+        alert('Salvo')
+        this.newTodo = ''
+      }
     },
     Excluir(id){
-      
       Todo.todoDel(id)
-        this.Listar()
-
-      
+      this.Listar()
     }
   }
+    
   
 }
   
@@ -92,5 +96,76 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped src="">
+h1{
+  height: 200px;
+}
+ul{
+  list-style-type: none;
+}
+label{
+  margin-right: 10px;
+}
 
+
+.meu-box
+{
+  position: relative;
+}
+
+.input-Titulo
+{
+  width: 70%;
+  position: relative;
+  font-size: 16px;
+  color: #5b5b5b;
+  padding: 10px 10px 10px 15px;
+  box-sizing: content-box;
+  z-index: 2;
+}
+
+.label-Titulo
+{
+  width: 70%;
+  position: absolute;
+  color: Goldenrod;
+  font-weight: bold;
+  top: 0; bottom: 0; left: 0; right: 0;
+  transition: 0.5s;                
+}
+
+.input-Titulo:focus + .label-Titulo
+{
+  margin-top: 0px;
+  top: -50%;
+  z-index: 2;
+  transition: 0.2s;  
+}
+.input-Todo
+{
+  width: 70%;
+  position: relative;
+  font-size: 16px;
+  color: #5b5b5b;
+  padding: 10px 10px 10px 15px;
+  box-sizing: content-box;
+  z-index: 2;
+}
+
+.label-Todo
+{
+  width: 70%;
+  position: absolute;
+  color: Goldenrod;
+  font-weight: bold;
+  top: 0; bottom: 0; left: 0; right: 0;
+  transition: 0.5s;                
+}
+
+.input-Todo:focus + .label-Todo
+{
+  margin-top: 0px;
+  top: -50%;
+  z-index: 2;
+  transition: 0.2s;  
+}
 </style>
